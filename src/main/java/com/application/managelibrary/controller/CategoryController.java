c3514ecaba91bc54464d49a001c063b03d3b0719
Ankit <ankitjaiswal885@gmail.com>
2022-08-03 18:09:57 +0530

    added presentation layer for Publisher and Category

---
+++ src/main/java/com/application/managelibrary/controller/CategoryController.java
@@ -0,0 +1,67 @@
+package com.application.managelibrary.controller;
+
+import com.application.managelibrary.entity.Category;
+import com.application.managelibrary.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.persistence.GeneratedValue;
+import java.util.List;
+
+@Controller
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public String findAllCategories(Model model){
+        List<Category> categories = categoryService.findAllCategories();
+        model.addAttribute("categories", categories);
+        return "categories";
+    }
+
+    @GetMapping("/update-category/{id}")
+    public String updateCategory(@PathVariable Long id, Model model){
+        Category category = categoryService.findCategoryById(id);
+        model.addAttribute("category", category);
+        return "update-category";
+    }
+
+    @PostMapping("/update-category/{id}")
+    public String updateCategory(@PathVariable Long id,Category category, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "update-category";
+        }
+        categoryService.updateCategory(category);
+        model.addAttribute("categories", categoryService.findAllCategories());
+        return "redirect:/categories";
+    }
+
+    @GetMapping("remove-category/{id}")
+    public String deleteCategory(@PathVariable Long id, Model model){
+        categoryService.deleteCategory(id);
+        model.addAttribute("categories", categoryService.findAllCategories());
+        return "categories";
+    }
+
+    @GetMapping("/add-category")
+    public String addCategory(Category category){
+        return "add-category";
+    }
+
+    @PostMapping("/save-category")
+    public String saveCategory(Category category, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "add-category";
+        }
+        categoryService.createCategory(category);
+        model.addAttribute("categories", category);
+        return "redirect:/categories";
+    }
+
+}
